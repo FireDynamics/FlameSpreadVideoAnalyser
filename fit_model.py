a45b17369d00217b311a49910c04da352bce7827
lu-kas <lukas.arnold@rub.de>
2017-09-08 11:34:36 +0200

    update optimizing method, rescale data

--- fit_model.py
+++ fit_model.py
@@ -14,8 +14,6 @@ def model_function(p, t):
     a2 = p[2]
     t2 = p[3]
 
-    res = np.zeros_like(t)
-
     res = y1 + a2 * (t-t1)
     res[t<t1] = y1
     res[t>t2] = y1 + a2 * (t2-t1)
@@ -27,18 +25,34 @@ def diff_function(p, data_t, data_p):
 
     res = np.sum((model_p - data_p)**2)
 
+    if p[0] < 0: res *= 2
+    if p[1] < 0: res *= 2
+
     return res
 
-data = np.loadtxt("output/results.csv", delimiter=';')
-data_t = data[:,0]
-data_p = data[:,1]
+data = np.loadtxt("output2/results.csv", delimiter=';')
 
-t_max = data_t[-1]
-p0 = np.array([0, 0.1*t_max, 1./(0.8*t_max), 0.9*t_max])
+t_max = data[-1,0]
+p_max = data[-1,1]
 
-result = scipy.optimize.minimize(diff_function, p0, (data_t, data_p))
+data_t = data[:,0] / t_max
+data_p = data[:,1] / p_max
+
+p0 = np.array([0, 0.1, 1./(0.8), 0.9])
+
+result = scipy.optimize.minimize(diff_function, p0, (data_t, data_p),
+                                 method = 'Nelder-Mead',
+                                 options={'maxiter':1000})
 
 p = result.x
+
+data_t *= t_max
+data_p *= p_max
+
+p[0] *= p_max
+p[1] *= t_max
+p[2] *= (p_max / t_max)
+p[3] *= t_max
 
 print result
 
@@ -48,4 +62,5 @@ plt.title("y1 = {:8.2e}; t1 = {:8.2e}; a2 = {:8.2e}; t2 = {:8.2e}".format(p[0], 
 plt.xlabel("time [s]")
 plt.ylabel("position [cm]")
 plt.legend()
-plt.savefig("output/fit.pdf")
+plt.savefig("output2/fit.pdf")
+plt.show()
